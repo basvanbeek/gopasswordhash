51d97fc8c03ff386ba697dd757cb2b4b14d59868
Bas van Beek <bas@tobin.nl>
2014-12-22 20:17:26 +0100

    added scrypt as optional key stretching algo in case we're worried about asic/fpga brute force attacks

--- PasswordHash.go
+++ PasswordHash.go
@@ -45,33 +45,46 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
-	
+	"golang.org/x/crypto/scrypt"
 	"golang.org/x/crypto/pbkdf2"
 )
 
-/* The PBKDF2_* constants may be changed without breaking existing stored hashes. */
-
-// PBKDF2_HASH_ALGORITHM can be set to sha1, sha224, sha256, sha384 or sha512 as the underlying hashing mechanism to be used by the PBKDF2 function
-const PBKDF2_HASH_ALGORITHM string = "sha512"
-// PBKDF2_ITERATIONS sets the amount of iterations used by the PBKDF2 hashing algorithm
-const PBKDF2_ITERATIONS     int    = 4096
-// PBKDF2_SALT_BYTES sets the amount of bytes for the salt used in the PBKDF2 hashing algorithm
-const PBKDF2_SALT_BYTES     int    = 64
-// PBKDF2_HASH_BYTES sets the amount of bytes for the hash output from the PBKDF2 hashing algorithm
-const PBKDF2_HASH_BYTES     int    = 64
+/* The PBKDF2_* and SCRYPT_* constants may be changed without breaking existing stored hashes. */
+const (
+	// PBKDF2_HASH_ALGORITHM can be set to sha1, sha224, sha256, sha384 or sha512 as the underlying hashing mechanism to be used by the PBKDF2 function
+	PBKDF2_HASH_ALGORITHM string = "sha512"
+	// PBKDF2_ITERATIONS sets the amount of iterations used by the PBKDF2 hashing algorithm
+	PBKDF2_ITERATIONS     int    = 15000
+	// PBKDF2_SALT_BYTES sets the amount of bytes for the salt used in the PBKDF2 hashing algorithm
+	PBKDF2_SALT_BYTES     int    = 64
+	// SCRYPT_N is a CPU/memory cost parameter, which must be a power of two greater than 1
+	SCRYPT_N              int    = 32768
+	// SCRYPT_R is the block size parameter
+	SCRYPT_R              int    = 8
+	// SCRYPT_P is the parallelization parameter, a positive integer less than or equal to ((2^32-1) * 32) / (128 * r)
+	SCRYPT_P              int    = 1
+	
+	// HASH_BYTES sets the amount of bytes for the hash output from the PBKDF2 / scrypt hashing algorithm
+	HASH_BYTES            int    = 64
+)
 
 /* altering the HASH_* constants breaks existing stored hashes */
-
-// HASH_SECTIONS identifies the expected amount of parameters encoded in a hash generated and/or tested in this package
-const HASH_SECTIONS         int    = 4
-// HASH_ALGORITHM_INDEX identifies the position of the hash algorithm identifier in a hash generated and/or tested in this package
-const HASH_ALGORITHM_INDEX  int    = 0
-// HASH_ITERATION_INDEX identifies the position of the iteration count used by PBKDF2 in a hash generated and/or tested in this package
-const HASH_ITERATION_INDEX  int    = 1
-// HASH_SALT_INDEX identifies the position of the used salt in a hash generated and/or tested in this package
-const HASH_SALT_INDEX       int    = 2
-// HASH_PBKDF2_INDEX identifies the position of the actual password hash in a hash generated and/or tested in this package
-const HASH_PBKDF2_INDEX     int    = 3
+const (
+	// HASH_SECTIONS identifies the expected amount of parameters encoded in a hash generated and/or tested in this package
+	HASH_SECTIONS         int    = 4
+	// HASH_ALGORITHM_INDEX identifies the position of the hash algorithm identifier in a hash generated and/or tested in this package
+	HASH_ALGORITHM_INDEX  int    = 0
+	// HASH_ITERATION_INDEX identifies the position of the iteration count used by PBKDF2 in a hash generated and/or tested in this package
+	HASH_ITERATION_INDEX  int    = 1
+	// HASH_SALT_INDEX identifies the position of the used salt in a hash generated and/or tested in this package
+	HASH_SALT_INDEX       int    = 2
+	// HASH_PBKDF2_INDEX identifies the position of the actual password hash in a hash generated and/or tested in this package
+	HASH_PBKDF2_INDEX     int    = 3
+	// HASH_SCRYPT_R_INDEX identifies the position of the scrypt block size parameter in a hash generated and/or tested in this package
+	HASH_SCRYPT_R_INDEX   int    = 4
+	// HASH_SCRYPT_R_INDEX identifies the position of the scrypt parallelization parameter in a hash generated and/or tested in this package
+	HASH_SCRYPT_P_INDEX   int    = 5
+)
 
 // CreateHash creates a salted cryptographic hash with key stretching (PBKDF2), suitable for storage and usage in password authentication mechanisms.
 func CreateHash(password string) (string, error) {
@@ -85,15 +98,26 @@ func CreateHash(password string) (string, error) {
 		default:
 			return "", errors.New("invalid hash algorithm selected")
 		case "sha1":
-			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, PBKDF2_HASH_BYTES, sha1.New)
+			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, HASH_BYTES, sha1.New)
 		case "sha224":
-			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, PBKDF2_HASH_BYTES, sha256.New224)
+			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, HASH_BYTES, sha256.New224)
 		case "sha256":
-			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, PBKDF2_HASH_BYTES, sha256.New)
+			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, HASH_BYTES, sha256.New)
 		case "sha384":
-			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, PBKDF2_HASH_BYTES, sha512.New384)
+			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, HASH_BYTES, sha512.New384)
 		case "sha512":
-			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, PBKDF2_HASH_BYTES, sha512.New)
+			hash = pbkdf2.Key([]byte(password), salt, PBKDF2_ITERATIONS, HASH_BYTES, sha512.New)
+		case "scrypt":
+			hash, err = scrypt.Key([]byte(password), salt, SCRYPT_N, SCRYPT_R, SCRYPT_P, HASH_BYTES) 
+			if err != nil {
+				return "", err
+			}
+			/* format: algorithm:cpu/mem cost:salt:hash:R(blocksize):P(parallelization) */
+			return fmt.Sprintf(
+				"%s:%d:%s:%s:%d:%d", PBKDF2_HASH_ALGORITHM, SCRYPT_N, 
+				base64.StdEncoding.EncodeToString(salt), base64.StdEncoding.EncodeToString(hash),
+				SCRYPT_R, SCRYPT_P,
+			), err
 	}
 
 	/* format: algorithm:iterations:salt:hash */
@@ -121,6 +145,7 @@ func ValidatePassword(password string, correctHash string) bool {
 	if err != nil {
 		return false
 	}
+
 	var testHash []byte
 	switch params[HASH_ALGORITHM_INDEX] {
 		default:
@@ -135,6 +160,16 @@ func ValidatePassword(password string, correctHash string) bool {
 			testHash = pbkdf2.Key([]byte(password), salt, it, len(hash), sha512.New384)
 		case "sha512":
 			testHash = pbkdf2.Key([]byte(password), salt, it, len(hash), sha512.New)
+		case "scrypt":
+			r, err := strconv.Atoi(params[HASH_SCRYPT_R_INDEX])
+			if err != nil {
+				return false
+			}
+			p, err := strconv.Atoi(params[HASH_SCRYPT_P_INDEX])
+			if err != nil {
+				return false
+			}
+			testHash, err = scrypt.Key([]byte(password), salt, it, r, p, len(hash))
 	}
 	return subtle.ConstantTimeCompare(hash, testHash) == 1
 }
